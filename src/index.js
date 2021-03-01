a3f1c77ae072e123048c0f5d3204c4ba96fbd38b
sergioivanov008 <sergioivanov008@gmail.com>
2021-03-01 18:44:17 +0300

    feat: implement towel-sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 module.exports = function towelSort(matrix) {
-    for (let i = 0; i < matrix.length; i++) {
 
+    let newMatrix = [];
+
+    if (matrix === undefined || matrix.length == 0) {
+        return newMatrix;
     }
-    return [];
+
+    for (let i = 0; i < matrix.length; i++) {
+        if ((i + 1) % 2 !== 0) {
+            for (let j = 0; j < matrix[i].length; j++) {
+                newMatrix.push(matrix[i][j]);
+            }
+
+        } else {
+            for (let j = matrix[i].length - 1; j >= 0; j--) {
+                newMatrix.push(matrix[i][j]);
+            }
+        }
+    }
+    return newMatrix;
 }
